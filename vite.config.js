60468a469d1e6acd73441e49f4c3ec8a3c9534ea
Umakantk8090 <uk8418805147@gmail.com>
2024-08-24 04:53:30 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"/react-redux/",
   plugins: [react()],
-  base:"/vite-react-deploy/",
+  
 })
